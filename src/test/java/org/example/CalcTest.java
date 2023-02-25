a3501b4e7aad252c5fe2b6edced0fbc29ca8b1f1
NadiaJoy <iminneverwhere@gmail.com>
2023-02-25 15:24:49 +0200

    Ex.1, ex.2

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -3,6 +3,8 @@ package org.example;
 
 import org.junit.*;
 
+import static jdk.nashorn.internal.objects.Global.Infinity;
+
 
 public class CalcTest {
     Calculator calculator;
@@ -13,25 +15,48 @@ public class CalcTest {
     }
 
     @Test
-    public void multPositiveNumbers () {
+    public void multPositiveNumbers() {
         double result = calculator.multiplication(2.2, 3.3);
         Assert.assertEquals(7.26, result, 0.001);
     }
+
     @Test
-    public void multNegariveNumbers () {
+    public void multNegariveNumbers() {
         double result = calculator.multiplication(-2.201, -3.305);
         Assert.assertEquals(7.274, result, 0.001);
     }
 
     @Test
-    public void multPositiveNegativeNumbers () {
+    public void multPositiveNegativeNumbers() {
         double result = calculator.multiplication(-1.4, 2);
         Assert.assertEquals(-2.8, result, 0.001);
     }
+
     @Test
-    public void multZero () {
+    public void multZero() {
         double result = calculator.multiplication(0, 3.3);
         Assert.assertEquals(0, result, 0.001);
+    }
 
+    @Test
+    public void divPositiveNumbers() {
+        double result = calculator.division(4.9, 1.5);
+        Assert.assertEquals(3.267, result, 0.001);
+    }
+
+    @Test
+    public void divNegativeNumbers() {
+        double result = calculator.division(-4.9, -1.5);
+        Assert.assertEquals(3.267, result, 0.001);
+    }
+    @Test
+    public void divZeroDividend() {
+        double result = calculator.division(0, -2.5);
+        Assert.assertEquals(0, result, 0.001);
+    }
+    @Test
+    public void divZeroDivision() {
+        double result = calculator.division(4.0, 0);
+        Assert.assertEquals(Infinity, result, 0.001);
     }
 }
